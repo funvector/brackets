362becc0ce78f220c58944e6902d7d211b266ab3
Vlad K <huza007@mail.com>
2019-03-24 23:03:22 +0300

    seat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var stack = [];
+  for (var i = 0; i < str.length; i++) {
+    var char = str.charAt(i);
+    var pair = getPair(char, bracketsConfig);
+    if (pair[0] === pair[1]) {
+      if (!stack || !stack.includes(char)) {
+        stack.push(char);
+      } else {
+        var pop = stack.pop();
+        if (pop != char) {
+          return false;
+        }
+      }
+    } else {
+      if (pair[0] === char) {
+        stack.push(char);
+      } else {
+        if (!stack) {
+          return false;
+        }
+
+        var pop = stack.pop();
+        if (pair[0] !== pop) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return stack.length == 0;
+}
+
+function getPair(symbol, bracketsConfig) {
+  var pair;
+  for (j = 0; j < bracketsConfig.length; j++) {
+    if (bracketsConfig[j][0] === symbol || bracketsConfig[j][1] === symbol) {
+      pair = bracketsConfig[j];
+    }
+  }
+
+  return pair;
 }
